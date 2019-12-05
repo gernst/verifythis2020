efce258035ac2f81891da4a625c667214f2e76e1
Gidon Ernst <gidon.ernst@sosy.ifi.lmu.de>
2019-12-05 14:51:22 +0100

    minor ideas

---
+++ src/main/scala/pgp/Attacker.scala
@@ -0,0 +1,8 @@
+package pgp
+
+/*
+ * Abstract model of an attacker that can observe the communication via the internet.
+ */
+class Attacker {
+  
+}
---
+++ src/main/scala/pgp/Client.scala
@@ -0,0 +1,13 @@
+package pgp
+
+/**
+ * Abstract model of the behavior of a client of the keyserver,
+ * i.e., someone holding a set of keys.
+ */
+class Client {
+  var keys: Map[Fingerprint, Key] = Map()
+
+  def receive(identity: Identity, email: EMail) {
+
+  }
+}
