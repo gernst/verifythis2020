9fd2a13f5ebb7c49a3d4804de636b1e742a6b7dc
Gidon Ernst <gidon.ernst@sosy.ifi.lmu.de>
2019-12-19 13:45:23 +0100

    simple model of sender/receiver and channels

--- src/main/scala/pgp/Types.scala
+++ src/main/scala/pgp/Types.scala
@@ -14,7 +14,6 @@ sealed trait Key {
 
 case class EMail(message: String, fingerprint: Fingerprint, token: Token)
 
-
 // Uses type 4 UUIDs with 122 bits of strong randomness.
 // Proposed by: https://github.com/wadoon/keyserver-java/
 case class Token(uuid: UUID)
@@ -23,4 +22,44 @@ object Token {
   def unique: Token = {
     Token(UUID.randomUUID)
   }
+}
+
+trait Send[A] {
+  def send(a: A)
+}
+
+trait Recv[A] {
+  def recv: Option[A]
+}
+
+sealed trait Actor[In,Out] {
+  def run(in: Recv[In], out: Send[Out]): Unit
+}
+
+object Channel {
+  def queue[A](): (Send[A], Recv[A]) = {
+    object ch extends Send[A] with Recv[A] {
+      import scala.collection.mutable
+      val msgs = mutable.Queue[A]()
+      def send(a: A) { msgs enqueue a }
+      def recv = if(msgs.isEmpty) None else Some(msgs.dequeue)
+    }
+    (ch, ch)
+  }
+
+  def _queue[A](): (Send[A], Recv[A]) = {
+    import scala.collection.mutable
+    val msgs = mutable.Queue[A]()
+
+    object send extends Send[A] {
+      def send(a: A) { msgs enqueue a }
+    }
+
+    object recv extends Recv[A] {
+      def recv = if(msgs.isEmpty) None else Some(msgs.dequeue)
+    }
+
+    (send, recv)
+  }
+
 }
