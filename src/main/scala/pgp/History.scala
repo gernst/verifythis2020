4eee94572b7ea21714138bdf4de7fc920645ccf1
Lukas Rieger <lukasrieger07@gmail.com>
2020-03-11 22:25:21 +0100

    Progress on History

--- src/main/scala/pgp/History.scala
+++ src/main/scala/pgp/History.scala
@@ -2,7 +2,6 @@ package pgp
 
 import scala.collection.mutable
 
-
 /**
  * Client erweitern,
  * Liste von Entscheidungen -> So wie Messages : Alle Entscheidungen "high level"
@@ -23,8 +22,6 @@ import scala.collection.mutable
  *
  *
  **/
-
-
 sealed trait Event
 
 case class Publish[T](given: T, fingerprint: Fingerprint) extends Event
@@ -45,30 +42,62 @@ case object Private extends Status
 
 case object Revoked extends Status
 
+
 trait State {
   type T
-  val verified: Set[Identity]
-  val associations: Map[Identity, Fingerprint]
 
-  def update(event: Event): State
+  def verified: Set[Identity]
+
+  def associations: Map[Identity, Fingerprint]
+
+  def update(event: Event): State = event match {
+    case Assoc(ids, fingerprint) =>
+      val ver = verified
+      val assoc = associations
+      new State {
+        def verified: Set[Identity] = ver
+
+        def associations: Map[Identity, Fingerprint] = ids.foldLeft(assoc) { (acc, v) => acc + (v -> fingerprint) }
+      }
+
+    case Verify(ids) => new State {
+      def verified = ???
+
+      def associations = ???
+    }
+    case Revoke(ids) => ???
+    case Publish(_, _) => ???
+  }
 }
 
 object History {
 
-  val history = mutable.Buffer[State]()
+  val history = mutable.Buffer[(Event, State)]()
 
-  def add(event: Event): State = {
-    history append (history.last update event)
-
-    history.last
+  def todo(event: Event): State = event match {
+    case Check => add(event); check()
+    case _ => add(event)
   }
 
-  def check() = {
+  def add(event: Event): State = {
+    val (_, state) = history.last
+    val next = state update event
+
+    history append ((event, next))
+
+    next
+  }
+
+  def check(): State = {
+
     /**
      * Step through history and collect knowledge about which identity is currently public/private etc.
      * At each point in the history, where the attacker inserted a Check-Event, we test, whether the given information
      * up to this point is sufficient to this point.
      */
+
+    ???
+
   }
 
 }
