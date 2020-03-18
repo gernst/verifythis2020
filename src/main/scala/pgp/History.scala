1fa88064e3d60414a20f1bb72d0b10b58a0f0c3d
Lukas Rieger <lukasrieger07@gmail.com>
2020-03-18 15:32:27 +0100

    notes

--- src/main/scala/pgp/History.scala
+++ src/main/scala/pgp/History.scala
@@ -11,7 +11,13 @@ import scala.collection.mutable
  *  1. Entscheidung: Fingerprint verknüpft mit Menge an Identities
  *  2. Revoke von Verbindungen Fingerprint und Identity
  *
+ * !!! History als Ablaufplan für Clients !!!
  *
+ * History wird von ScalaCheck generiert.
+ *
+ * 2 Arten die History zu interpretieren:
+ * -> Actor die die Events ausführen.
+ * -> Checker, der nur die History verarbeitet.
  *
  * Auf Angreifer Seite,
  *
@@ -20,19 +26,37 @@ import scala.collection.mutable
  * Angreifer vlt kein direkter Zugriff, stattdessen auch Messages in der Historie.
  * -> Prüfen, ob Historie so erlaubt ist.
  *
+ * Egal über Fingerprint/KeyId/Email -> Immer Key filtern
+ *
+ * Funktionale Eigenschaften: Key ist selber bei Upload und download
+ *
+ *
+ * Gegeben fingerprint -> Public identities auf basis der History zurückgeben.
+ *
+ *
+ * Aufschreiben.
  *
  **/
+
 sealed trait Event
 
-case class Publish[T](given: T, fingerprint: Fingerprint) extends Event
+/*
+  Durch Histoy durchlaufen und sich Verify und Revoke anschauen.
 
-case class Revoke(ids: Set[Identity]) extends Event
+*/
 
+// das kann weg
+case class Publish(fingerprint: Fingerprint) extends Event
+
+case class Revoke(ids: Set[Identity], fingerprint: Fingerprint) extends Event
+
+// vlt einfach Key als parameter
 case class Assoc(ids: Set[Identity], fingerprint: Fingerprint) extends Event
 
-case class Verify(ids: Set[Identity])
+case class Verify(ids: Set[Identity], fingerprint: Fingerprint) extends Event
 
 case object Check extends Event
+
 
 sealed trait Status
 
@@ -70,12 +94,19 @@ trait State {
   }
 }
 
+
+/**
+ * Ohne State,
+ * Überprüfen sollte separat bleiben
+ */
+
 object History {
 
   val history = mutable.Buffer[(Event, State)]()
 
+
   def todo(event: Event): State = event match {
-    case Check => add(event); check()
+    case Check => add(event); check(???, ???)
     case _ => add(event)
   }
 
@@ -88,7 +119,7 @@ object History {
     next
   }
 
-  def check(): State = {
+  def check(queryResult: Key, history: this.type): State = {
 
     /**
      * Step through history and collect knowledge about which identity is currently public/private etc.
